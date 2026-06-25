f3765ee429605157335d228851d4c7e3cfd0fc91
RAHUMAN-07 <rahumanlmmcabdul@gmail.com>
2026-06-25 22:35:19 +0530

    commit 1

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,9 +1,17 @@
 import env from './env.js';
 
+const allowedOrigins = [env.FRONTEND_URL, 'http://localhost:5173'];
+
 export const corsOptions = {
-  origin: env.FRONTEND_URL,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS policy: origin ${origin} not allowed`));
+    }
+  },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
